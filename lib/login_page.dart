faf8325dd9cc46a3962b3dba2ff52559a9dff75a
Karl-Gilmartin <karl_gilmartin@hotmail.com>
2024-02-25 18:19:15 +0000

    new login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,53 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:street_wise/home_page.dart';
-import 'package:street_wise/report_page.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("StreetWise"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/images/streetwise_logo_black.png',
-                width: 200, height: 200, fit: BoxFit.contain),
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+      body: Column(
+        children: [
+          // Top container with logo and white background
+          Expanded(
+            flex:
+                2, // Adjust flex factor to change the space ratio of the top container
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.purple.shade900,
+                borderRadius: BorderRadius.circular(20), // Adjust the radius value as per your preference
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // Logo
+                    Image.asset(
+                      'assets/images/streetwise_logo_white.png', // Replace with your logo
+                      width: 200,
+                      height: 200,
+                    ),
+                    SizedBox(height: 24),
+                    Text(
+                      'Navigate Safe, Navigate Smart', // Replace with your app name
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    // You can add your login text form fields here
+                  ],
+                ),
+              ),
             ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
+          ),
+          // Bottom container with purple background
+          Expanded(
+            flex:
+                2, // Adjust flex factor to change the space ratio of the bottom container
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(
+                    10), // Adjust the radius value as per your preference
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(25), // Add desired padding value
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(25), // Add desired padding value
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                      ),
+                    ),
+                    // Add additional widgets or text if you want
+                    
+                    SizedBox(height: 24),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Don\'t have an account?',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            // Handle your register logic here
+                          },
+                          child: Text('Register'),
+                        ),
+                      ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle your login logic here
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.purple.shade900, // Button background color
+                        onPrimary: Colors.white, // Button text color
+                      ),
+                      child: Text('Login'),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to next page
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ReportPage(name:_usernameController.text)),
-                );
-              },
-              child: Text('Login'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
